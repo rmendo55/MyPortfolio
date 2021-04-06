a7684009c40e1b72dd95a2f8fbe3f1066054c07b
rmendo55 <mendozarafael914@gmail.com>
2021-04-06 01:09:11 -0700

    Add Home Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navigation from './components/Navigation'
+import Home from './components/Home'
 import "./App.css"
 
 function App() {
   return (
     <div>
       <Navigation />
+      <Home />
     </div>
   );
 }
