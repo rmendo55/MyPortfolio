10eaff37e52000386dd5abeab606019d6bbdaa9f
rmendo55 <mendozarafael914@gmail.com>
2021-04-07 21:35:05 -0700

    Add Footer Component to App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navigation from './components/Navigation'
 import Home from './components/Home'
+import Footer from './components/Footer'
 import "./App.css"
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div>
       <Navigation />
       <Home />
+      <Footer />
     </div>
   );
 }
