689b45cf7bfcf13a8221fd2c92e015935a2ff0fa
rmendo55 <mendozarafael914@gmail.com>
2021-04-13 01:01:13 -0700

    Make home and about txt responsive

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,8 +4,8 @@ function Home() {
     return (
         <div className="home-container">
             <div className="home-txt-container">
-                <h3>Hello! I'm Rafael Mendoza Welcome to my website</h3>
-                <h4>I am a Software Engineer & Quality Assurance Engineer</h4>
+                <h3 style={{fontSize:"3vw"}}>Hello! I'm Rafael Mendoza Welcome to my website</h3>
+                <h4 style={{fontSize:"3vw"}}>I am a Software Engineer & Quality Assurance Engineer</h4>
             </div>
             <div className="home-img-container">
                 <img src="homeImg2.png"  alt="Home image" className="home-img"/>
