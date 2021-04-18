f39e42621dde22d071bb23d6e8b4fa791eae85cb
rmendo55 <mendozarafael914@gmail.com>
2021-04-18 15:37:39 -0700

    Making project section responsive using media query

--- src/components/Thumbnail.js
+++ src/components/Thumbnail.js
@@ -2,13 +2,19 @@ import React from 'react'
 
 function Thumbnail(props) {
     return(
-        <div className="card" style={{width:"25vw"}}>
-            <img className="card-img-top" src={props.src} alt={props.alt} style={{width:"100%"}}/>
+        <div className="card">
+            <img className="card-img-top" src={props.src} alt={props.alt}/>
             <div className="card-body">
-                <h4 className="card-title" style={{color: "black", textAlign: "center", fontSize: "1.5vw", marginBottom: "10%"}}>{props.label}</h4>
-                <p className="card-text" style={{color: "black", fontSize: "1.2vw", marginBottom: "10%"}}>{props.description}</p>
-                <a href="#" className="btn btn-primary" id="view-btn" style={{fontSize: "1.2vw"}}>View</a>
-                <a href="#" className="btn btn-primary" id="view-btn" style={{fontSize: "1.2vw"}}>View Code</a>
+                <div className="card-title">
+                    <h4 style={{color: "black", textAlign: "center", fontSize: "1.5vw"}}>{props.label}</h4>
+                </div>
+                <div className="card-txt">
+                    <p style={{color: "black", fontSize: "1.2vw"}}>{props.description}</p>
+                </div>
+                <div className="project-buttons">
+                    <a href="#" className="btn btn-primary" id="view-btn" style={{fontSize: "1.2vw"}}>View</a>
+                    <a href="#" className="btn btn-primary" id="view-btn" style={{fontSize: "1.2vw"}}>View Code</a>
+                </div>
             </div>
         </div>
     )
