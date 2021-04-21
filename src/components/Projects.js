652726e399ba5966b116d5393e1799ed99c123d5
rmendo55 <mendozarafael914@gmail.com>
2021-04-21 01:16:07 -0700

    Make thumbnail content responsive

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -10,51 +10,51 @@ function Projects() {
     }, []);
     return(
         <div>
-            <Header title="Projects" /> 
+            <Header title="Projects" />
             <div className="projects">
                 <div className="thumbnail-container">
                     <div data-aos="fade-right" className="thumbnail-content">
-                        <Thumbnail 
-                        src="myimages/main_page.png" 
-                        alt="Fusion Colors Main" 
+                        <Thumbnail
+                        src="myimages/main_page.png"
+                        alt="Fusion Colors Main"
                         label="Fusion Colors Database"
-                        description="Register incoming car parts for Fusion Colors located at Santa Maria, CA. 
+                        description="Register incoming car parts for Fusion Colors located at Santa Maria, CA.
                         Can search, delete, upload images, and update inventory."
                         />
                     </div>
                     <div data-aos="fade-up" className="thumbnail-content">
-                        <Thumbnail 
-                        src="myimages/job_search_main.png" 
-                        alt="Job Search Main" 
+                        <Thumbnail
+                        src="myimages/job_search_main.png"
+                        alt="Job Search Main"
                         label="Job Search"
-                        description="Develop a web application that allows a user to search for jobs and 
+                        description="Develop a web application that allows a user to search for jobs and
                         view all jobs related to that job search. Used a public API."
                         />
                     </div>
                     <div data-aos="fade-left" className="thumbnail-content">
-                        <Thumbnail 
-                        src="myimages/sort_algo_graph.png" 
-                        alt="Sorting Algorithm Main" 
+                        <Thumbnail
+                        src="myimages/sort_algo_graph.png"
+                        alt="Sorting Algorithm Main"
                         label="Sorting Algorithm Comparison"
-                        description="Develop a program that implements different sorting algorithms and 
+                        description="Develop a program that implements different sorting algorithms and
                         then compares all sorting algorithms on different sets of data."
                         />
                     </div>
                 </div>
                 <div className="thumbnail-container">
                     <div data-aos="fade-right" className="thumbnail-content">
-                        <Thumbnail 
-                        src="myimages/memorygame.png" 
-                        alt="Memory Game Main" 
+                        <Thumbnail
+                        src="myimages/memorygame.png"
+                        alt="Memory Game Main"
                         label="Memory Game"
-                        description="Developed a single player version JavaFx game where user is to turn 
+                        description="Developed a single player version JavaFx game where user is to turn
                         over pairs of matching cards."
                         />
                     </div>
                     <div data-aos="fade-left" className="thumbnail-content">
-                        <Thumbnail 
-                        src="myimages/mazepath.png" 
-                        alt="Stack Maze Solver Main" 
+                        <Thumbnail
+                        src="myimages/mazepath.png"
+                        alt="Stack Maze Solver Main"
                         label="Stack Maze Solver"
                         description="Implemented a maze solver program that utilizes a stack."
                         />
--- src/components/Thumbnail.js
+++ src/components/Thumbnail.js
@@ -6,18 +6,18 @@ function Thumbnail(props) {
             <img className="card-img-top" src={props.src} alt={props.alt}/>
             <div className="card-body">
                 <div className="card-title">
-                    <h4 style={{color: "black", textAlign: "center", fontSize: "1.5vw"}}>{props.label}</h4>
+                    <h4>{props.label}</h4>
                 </div>
                 <div className="card-txt">
-                    <p style={{color: "black", fontSize: "1.2vw"}}>{props.description}</p>
+                    <p>{props.description}</p>
                 </div>
                 <div className="project-buttons">
-                    <a href="#" className="btn btn-primary" id="view-btn" style={{fontSize: "1.2vw"}}>View</a>
-                    <a href="#" className="btn btn-primary" id="view-btn" style={{fontSize: "1.2vw"}}>View Code</a>
+                    <a href="#" className="btn btn-primary view-button" id="view-btn">View</a>
+                    <a href="#" className="btn btn-primary view-button" id="view-btn">View Code</a>
                 </div>
             </div>
         </div>
     )
 }
 
-export default Thumbnail
+export default Thumbnail;
