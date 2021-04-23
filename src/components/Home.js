9ce991c84e81467337d0583044d356e379cacaa2
rmendo55 <mendozarafael914@gmail.com>
2021-04-22 17:52:07 -0700

    adjust all sections

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,8 +4,8 @@ function Home() {
     return (
         <div className="home-container">
             <div className="home-txt-container">
-                <h3 style={{fontSize:"3vw"}}>Hello! I'm Rafael Mendoza Welcome to my website</h3>
-                <h4 style={{fontSize:"3vw"}}>I am a Software Engineer & Quality Assurance Engineer</h4>
+                <h1 className="home-txt1">Hello! I'm Rafael Mendoza Welcome to my website</h1>
+                <h4 className="home-txt2">I am a Software Engineer & Quality Assurance Engineer</h4>
             </div>
             <div className="home-img-container">
                 <img src="myimages/homeImg2.png"  alt="Home image" className="home-img"/>
