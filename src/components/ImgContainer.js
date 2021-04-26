0d42f3afbe0d888b5f5b8cdafd7a8685494cf42e
rmendo55 <mendozarafael914@gmail.com>
2021-04-26 01:42:42 -0700

    Change img path

--- src/components/ImgContainer.js
+++ src/components/ImgContainer.js
@@ -5,21 +5,21 @@ function ImgContainer() {
 	return(
 		<div className="img-container">
 			<div className="first-row">
-				<ImgComponent name="myImages/kotlin.jpg"/>
-				<ImgComponent name="myImages/java.webp"/>
-				<ImgComponent name="myImages/python.png"/>
-				<ImgComponent name="myImages/javascript.png"/>
+				<ImgComponent name="./myImages/kotlin.jpg"/>
+				<ImgComponent name="./myImages/java.webp"/>
+				<ImgComponent name="./myImages/python.png"/>
+				<ImgComponent name="./myImages/javascript.png"/>
 			</div>
 			<div className="second-row">
-				<ImgComponent name="myImages/html.png"/>
-				<ImgComponent name="myImages/css.webp"/>
-				<ImgComponent name="myImages/bootstrap.png"/>
-				<ImgComponent name="myImages/android.png"/>
+				<ImgComponent name="./myImages/html.png"/>
+				<ImgComponent name="./myImages/css.webp"/>
+				<ImgComponent name="./myImages/bootstrap.png"/>
+				<ImgComponent name="./myImages/android.png"/>
 			</div>
 			<div className="third-row">
-				<ImgComponent name="myImages/postgres.jpeg"/>
-				<ImgComponent name="myImages/php.svg"/>
-				<ImgComponent name="myImages/mysql.png"/>
+				<ImgComponent name="./myImages/postgres.jpeg"/>
+				<ImgComponent name="./myImages/php.svg"/>
+				<ImgComponent name="./myImages/mysql.png"/>
 			</div>
 		</div>
 	)
